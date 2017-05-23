5ed8f0ac9453bf1632ce8992ea3c25d4d4eb6a20
Kerzzi <kerzzi@outlook.com>
2017-05-23 18:59:06 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -1,17 +1,5 @@
 class Account::OrdersController < ApplicationController
 
-  layout "admin"
 
-  before_action :authenticate_user!
-  before_action :admin_required
-
-  def index
-    @orders = current_user.orders.order("id DESC")
-  end
-
-  def show
-    @order = Order.find(params[:id])
-    @product_lists = @order.product_lists
-  end
 
 end
