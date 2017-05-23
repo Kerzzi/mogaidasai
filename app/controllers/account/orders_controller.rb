79d349034189bad40b1e1afcdb0e851f717fd9c9
Kerzzi <kerzzi@outlook.com>
2017-05-23 18:55:50 +0800

    implement admin/orders show function

--- app/controllers/account/orders_controller.rb
+++ app/controllers/account/orders_controller.rb
@@ -8,4 +8,10 @@ class Account::OrdersController < ApplicationController
   def index
     @orders = current_user.orders.order("id DESC")
   end
+
+  def show
+    @order = Order.find(params[:id])
+    @product_lists = @order.product_lists
+  end
+
 end
