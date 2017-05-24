55cbf3826c1fe49eb61d1ea8f55a3fee3039cd42
Kerzzi <kerzzi@outlook.com>
2017-05-24 23:39:33 +0800

    figaro

--- Gemfile
+++ Gemfile
@@ -49,6 +49,7 @@ gem 'paperclip'
 gem 'acts_as_votable', '~> 0.10.0'
 gem 'carrierwave-qiniu'
 gem 'qiniu-rs'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,10 +1,10 @@
 
 CarrierWave.configure do |config|
   config.storage             = :qiniu
-  config.qiniu_access_key    = ENV["TYBurzNuy6Rok1mdl9Q63AJyPxDcPK7FEA4MWurB"]
-  config.qiniu_secret_key    = ENV["Q81jNwVOIgLlHBeIWHhMib9BtBR4F5gA-udUa-pz"]
-  config.qiniu_bucket        = ENV["jdstore"]
-  config.qiniu_bucket_domain = ENV["oqgnjaxh1.bkt.clouddn.com"]
+  config.qiniu_access_key    = ENV["qiniu_access_key"]
+  config.qiniu_secret_key    = ENV["qiniu_secret_key"]
+  config.qiniu_bucket        = ENV["qiniu_bucket"]
+  config.qiniu_bucket_domain = ENV["qiniu_bucket_domain"]
   config.qiniu_block_size    = 4*1024*1024
   config.qiniu_protocol      = "http"
   config.qiniu_up_host       = "http://up.qiniug.com"  #选择不同的区域时，"up.qiniug.com" 不同
